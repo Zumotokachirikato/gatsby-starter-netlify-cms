31ca1e603bfe3aa4bb66af871d8c3451d9551a8d
Vision-X <michael.s.marlow@gmail.com>
2019-07-08 02:27:18 -0600

    adjust style for bulma classes on main header box

--- src/components/BlogRoll.js
+++ src/components/BlogRoll.js
@@ -7,72 +7,13 @@ class BlogRoll extends React.Component {
   render() {
     const { data } = this.props
     const { edges: posts } = data.allMarkdownRemark
-    // let datey = post.frontmatter.date1.slice(0, post.frontmatter.date1.indexOf(","))
 
     return (
       <div className="columns is-multiline">
         {posts &&
           posts
-          .sort((a, b) => {
-            b.post.frontmatter.date1.slice(0, post.frontmatter.date1.indexOf(","))
-             -
-            a.post.frontmatter.date1.slice(0, post.frontmatter.date1.indexOf(","))
-          })
           .map(({ node: post }) => (
-            post.frontmatter.featuredpost ?
-              (<div className="is-parent column is-12" key={post.id}>
-                <article
-                  className={`blog-list-item tile is-child box notification ${
-                    post.frontmatter.featuredpost ? 'is-featured' : ''
-                  }`}
-                >
-                  <header>
-                    {post.frontmatter.featuredimage ? (
-                      <div className="featured-thumbnail">
-                        <PreviewCompatibleImage
-                          imageInfo={{
-                            image: post.frontmatter.featuredimage,
-                            alt: `featured image thumbnail for post ${
-                              post.title
-                            }`,
-                          }}
-                        />
-                      </div>
-                    ) : null}
-                    <p className="post-meta">
-                      <Link
-                        className="title has-text-primary is-size-4"
-                        to={post.fields.slug}
-                      >
-                        {post.frontmatter.title}
-                      </Link>
-                    {/*</p>*/}
-                      <br />
-                      <span className="subtitle is-size-5 is-block">
-                          {(post.frontmatter.date1 && post.frontmatter.date2) &&
-                            (post.frontmatter.date2.slice(0, post.frontmatter.date2.indexOf(","))
-                              !== post.frontmatter.date1.slice(0, post.frontmatter.date1.indexOf(",")))
-                            ? `${post.frontmatter.date1.slice(0, post.frontmatter.date1.length - 16)} - ${post.frontmatter.date2.slice(0, post.frontmatter.date2.length - 9) }`
-                            : `${post.frontmatter.date1} - ${post.frontmatter.date2.slice(17, post.frontmatter.date2.length)}`
-                          }
-                      </span>
-                    </p>
-                  </header>
-                  <p>
-                    {post.excerpt}
-                    <br />
-                    <br />
-                    <Link className="button" to={post.fields.slug}>
-                      More Info →
-                    </Link>
-                    <a className="button tickets-btn" href="https://app.arts-people.com/index.php?ticketing=pvp" style={{marginLeft: 10 + 'px'}}>
-                      Tickets
-                    </a>
-                  </p>
-                </article>
-              </div>)
-              :
-              (<div className="is-parent column is-6" key={post.id}>
+              <div className="is-parent column is-6" key={post.id}>
                 <article
                   className={`blog-list-item tile is-child box notification ${
                     post.frontmatter.featuredpost ? 'is-featured' : ''
@@ -103,7 +44,7 @@ class BlogRoll extends React.Component {
                           {(post.frontmatter.date1 && post.frontmatter.date2) &&
                             (post.frontmatter.date2.slice(0, post.frontmatter.date2.indexOf(","))
                               !== post.frontmatter.date1.slice(0, post.frontmatter.date1.indexOf(",")))
-                            ? `${post.frontmatter.date1.slice(0, post.frontmatter.date1.length - 15)} - ${post.frontmatter.date2.slice(0, post.frontmatter.date2.length - 9) }`
+                            ? `${post.frontmatter.date1.slice(0, post.frontmatter.date1.length - 15)} - ${post.frontmatter.date2.slice(0, post.frontmatter.date2.length - 9)}`
                             : `${post.frontmatter.date1} - ${post.frontmatter.date2.slice(17, post.frontmatter.date2.length)}`
                           }
                       </span>
@@ -121,7 +62,7 @@ class BlogRoll extends React.Component {
                     </a>
                   </p>
                 </article>
-              </div>)
+              </div>
             ))}
 
       </div>
@@ -155,12 +96,12 @@ export default () => (
               frontmatter {
                 title
                 templateKey
-                date1(formatString: "MMMM DD, YYYY, h:mm a")
-                date2(formatString: "MMMM DD, YYYY, h:mm a")
+                date1(formatString: "MMMM DD, YYYY, h:mm A")
+                date2(formatString: "MMMM DD, YYYY, h:mm A")
                 featuredpost
                 featuredimage {
                   childImageSharp {
-                    fluid(maxWidth: 120, quality: 100) {
+                    fluid(maxWidth: 480, quality: 100) {
                       ...GatsbyImageSharpFluid
                     }
                   }
