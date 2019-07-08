823cd966b4b9f489bde659f89d442c5cb279794d
Vision-X <michael.s.marlow@gmail.com>
2019-07-08 07:24:01 -0600

    add favicon set for helmet/head, import files, update Layout.js, improve header css

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -14,7 +14,11 @@ const TemplateWrapper = ({ children }) => {
         <title>{title}</title>
         <meta name="description" content={description} />
         <meta name="viewport" content="width=device-width, initial-scale = 1.0, maximum-scale=1.0, user-scalable=no" />
-
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+        <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
+        <meta name="msapplication-TileColor" content="#da532c" />
         <link
           rel="apple-touch-icon"
           sizes="180x180"
@@ -43,7 +47,7 @@ const TemplateWrapper = ({ children }) => {
         <meta property="og:type" content="business.business" />
         <meta property="og:title" content={title} />
         <meta property="og:url" content="/" />
-        <meta property="og:image" content="/img/og-image.jpg" />
+        <meta property="og:image" content="/img/android-chrome-256x256.png" />
       </Helmet>
       <Navbar />
       <div>{children}</div>
